c3fc71701b1c8f0174670a7f48850923bedf42c0
NVENTIVE\felix.brault <felix.brault@nventive.com>
2022-09-15 15:38:23 -0400

    refactor: Fix building sdk

--- src/SectionsNavigation.Abstractions/AssemblyInfo.cs
+++ src/SectionsNavigation.Abstractions/AssemblyInfo.cs
@@ -3,4 +3,3 @@
 // What is internal in SectionsNavigation.Abstractions will be accessible in Chinook.SectionsNavigation, etc.
 [assembly: InternalsVisibleTo("Chinook.SectionsNavigation")]
 [assembly: InternalsVisibleTo("Chinook.SectionsNavigation.Uno")]
-[assembly: InternalsVisibleTo("Chinook.SectionsNavigation.Uno.WinUI")]
