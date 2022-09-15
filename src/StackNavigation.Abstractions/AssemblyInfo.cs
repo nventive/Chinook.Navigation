744a9841d6d7183f7d2bf2063bf2dc4415c29b9f
NVENTIVE\felix.brault <felix.brault@nventive.com>
2022-09-15 15:53:20 -0400

    refactor: Reference abstraction assembly with WinUI

--- src/StackNavigation.Abstractions/AssemblyInfo.cs
+++ src/StackNavigation.Abstractions/AssemblyInfo.cs
@@ -3,3 +3,4 @@
 // What is internal in StackNavigation.Abstractions will be accessible in Chinook.StackNavigation, etc.
 [assembly: InternalsVisibleTo("Chinook.StackNavigation")]
 [assembly: InternalsVisibleTo("Chinook.StackNavigation.Uno")]
+[assembly: InternalsVisibleTo("Chinook.StackNavigation.Uno.WinUI")]
