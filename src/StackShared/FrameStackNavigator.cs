58170b89a8f433a24a13321a8e1723f79497002c
Carl Mathieu <ch.mathieu@nventive.com>
2022-10-05 18:15:43 -0400

    chore: cleanup projet

--- src/StackShared/FrameStackNavigator.cs
+++ src/StackShared/FrameStackNavigator.cs
@@ -291,7 +291,7 @@ namespace Chinook.StackNavigation
 
 					var result = await WaitForNavigationResult(() => _frame.GoBack(overrideInfo));
 
-#if NETFX_CORE
+#if WINDOWS_UWP || WINDOWS
 					// On Windows, Frame re-creates a new Page instance for every navigation (whether through Navigate or GoBack),
 					// unless NavigationCacheMode is set to Required (in which case a single Page instance is recycled).
 					if (result.Content != activeEntry.View)
